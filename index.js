555f7decd78d6d7f1d299e2a30c2b9f449253e20
Ogbe-cloud <ogbeedel10000@gmail.com>
2024-04-18 03:33:12 -0700

    committing changes before switching branches

---
+++ index.js
@@ -0,0 +1,74 @@
+let computerScore = 0;
+let playerScore = 0;
+
+
+
+let result = document.querySelector('.winner');
+
+function getComputerChoice() {
+    let computer = Math.random();
+    let computerChoice;
+    if (computer < 0.45) {
+        computerChoice = 'rock';
+    } else if (computer >= 0.45 && computer < 0.75) {
+        computerChoice = 'paper';
+    } else {
+        computerChoice = 'scissors';
+    }
+    return computerChoice;
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return 'It is a tie';
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+        playerScore++;
+        return 'You win';
+    } else {
+        computerScore++;
+        return 'You lose';
+    }
+}
+
+function updateScoreDisplay() {
+    result.textContent = `Player Score: ${playerScore}, Computer Score: ${computerScore}`;
+    if (playerScore === 5) {
+        alert('You win the game! 🏆');
+		playerScore = 0;
+    } else if (computerScore === 5) {
+        alert('Computer wins the game! 🏆');
+		computerScore = 0;
+    }
+}
+
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+
+rock.addEventListener('click', () => {
+    const computerSelection = getComputerChoice();
+    const playerSelection = 'rock';
+    const roundResult = playRound(playerSelection, computerSelection);
+    result.textContent = roundResult;
+    updateScoreDisplay();
+});
+
+paper.addEventListener('click', () => {
+    const computerSelection = getComputerChoice();
+    const playerSelection = 'paper';
+    const roundResult = playRound(playerSelection, computerSelection);
+    result.textContent = roundResult;
+    updateScoreDisplay();
+});
+
+scissors.addEventListener('click', () => {
+    const computerSelection = getComputerChoice();
+    const playerSelection = 'scissors';
+    const roundResult = playRound(playerSelection, computerSelection);
+    result.textContent = roundResult;
+    updateScoreDisplay();
+});
